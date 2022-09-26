153c71cdd6ba549caea7e22a1e28932a54374247
kelvin2007 <kelvin.erlangga@ui.ac.id>
2022-09-26 10:52:28 +0700

    feat: tugas 1 - initialize spring boot project

---
+++ src/main/java/apap/tugas/tugas1_singidol_2006596964/Tugas1Singidol2006596964Application.java
@@ -0,0 +1,13 @@
+package apap.tugas.tugas1_singidol_2006596964;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas1Singidol2006596964Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas1Singidol2006596964Application.class, args);
+	}
+
+}
---
+++ src/test/java/apap/tugas/tugas1_singidol_2006596964/Tugas1Singidol2006596964ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugas.tugas1_singidol_2006596964;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas1Singidol2006596964ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
